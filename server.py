93fc3fa91b73295bdd073ef5f6cc3f1f680f34a9
honozon <32091351+honozon@users.noreply.github.com>
2020-06-14 19:36:06 +0200

    use env variable port
    to be able to deploy to heroku

--- server.py
+++ server.py
@@ -70,5 +70,5 @@ def resultatbdd():
 # run the application
 
 if __name__ == "__main__":  
-    app.run(host='0.0.0.0',debug=True,port=8080)
+    app.run(host='0.0.0.0',debug=True,port=os.getenv("PORT", 8080))
     
